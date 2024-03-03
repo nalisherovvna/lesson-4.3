06c9de195b76febee200c2de5c365c902f4e26ca
nalisherovvna <mushknasiba@gmail.com>
2024-03-03 18:10:24 +0200

    it was my homework

---
+++ main.js
@@ -0,0 +1,16 @@
+let numberFirst = Number(prompt("enter first number = "));
+let numberSecond = Number(prompt("enter second number = "));
+
+let sum, a , b ,c;
+
+
+sum = numberFirst + numberSecond;
+a = numberFirst / numberSecond;
+b = numberFirst - numberSecond;
+c = numberFirst * numberSecond;
+
+
+console.log(sum);
+console.log(a);
+console.log(b);
+console.log(c);
